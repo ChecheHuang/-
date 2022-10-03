d89600fdc648421247855d8712eed6266f49bcf3
Carl <carlhuang@sunnygo.com.tw>
2022-10-03 13:21:17 +0800

    分裝及增加level和scorePanel

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,7 @@
-import './style/index.less';
+import "./style/index.less";
+import Food from "./modules/Food";
+import ScorePanel from "./modules/ScorePanel";
 
-class Food{
 
-    element:HTMLElement;
-
-    constructor(){
-        this.element=document.getElementById("food")!;
-    }
-
-    get X(){
-        return this.element.offsetLeft;
-    }
-
-    get Y(){
-        return this.element.offsetTop;
-    }
-
-    change(){
-
-        let top = Math.round(Math.random()*29)*10
-        let left = Math.round(Math.random()*29)*10
-
-        this.element.style.left = left+'px'
-        this.element.style.top = top+'px'
-    }
-
-}
 
 const food = new Food();
-console.log(food.X,food.Y)
-food.change()
-console.log(food.X,food.Y)
