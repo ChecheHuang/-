465f589550bd604887ccc2d1399e274a44501652
Carl <carlhuang@sunnygo.com.tw>
2022-10-05 13:22:54 +0800

    讓蛇移動及撞牆和吃東西

--- src/modules/GameControl.ts
+++ src/modules/GameControl.ts
@@ -6,7 +6,8 @@ class GameControl {
   snake: Snake;
   food: Food;
   scorePanel: ScorePanel;
-  direction: string = "";
+  direction: string = "Right";
+  isLive = true;
 
   constructor() {
     this.snake = new Snake();
@@ -17,11 +18,59 @@ class GameControl {
 
   init() {
     document.addEventListener("keydown", this.keydownHandler.bind(this));
+    this.run();
   }
 
   keydownHandler(event: KeyboardEvent) {
-    console.log(event.key);
-    this.direction = event.key
+    // console.log(event.key);
+    this.direction = event.key;
+  }
+
+  run() {
+    let X = this.snake.X;
+    let Y = this.snake.Y;
+
+    switch (this.direction) {
+      case "ArrowUp":
+      case "Up":
+        Y -= 10;
+        break;
+      case "ArrowDown":
+      case "Down":
+        Y += 10;
+        break;
+      case "ArrowLeft":
+      case "Left":
+        X -= 10;
+        break;
+      case "ArrowRight":
+      case "Right":
+        X += 10;
+        break;
+      default:
+        break;
+    }
+
+    this.checkEat(X, Y);
+
+    try {
+      this.snake.X = X;
+      this.snake.Y = Y;
+    } catch (err) {
+      alert(err);
+      this.isLive = false;
+    }
+
+    this.isLive &&
+      setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30);
+  }
+
+  checkEat(X: number, Y: number) {
+    if (X === this.food.X && Y === this.food.Y) {
+      this.food.change();
+      this.scorePanel.addScore();
+      this.snake.addBody();
+    }
   }
 }
 
--- src/modules/Snake.ts
+++ src/modules/Snake.ts
@@ -2,10 +2,10 @@ class Snake {
   head: HTMLElement;
   bodies: HTMLCollection;
 
-  element:HTMLElement; 
+  element: HTMLElement;
 
   constructor() {
-    this.element=document.getElementById("snake")!;
+    this.element = document.getElementById("snake")!;
     this.head = document.querySelector("#snake>div") as HTMLElement;
     this.bodies = document.getElementById("snake")!.getElementsByTagName("div");
   }
@@ -18,17 +18,30 @@ class Snake {
     return this.head.offsetTop;
   }
 
-  set X(value:number){
-    this.head.style.left = value+"px"
+  set X(value: number) {
+    if (this.X === value) {
+      return;
+    }
+
+    if (value < 0 || value > 290) {
+      throw new Error("蛇撞牆了");
+    }
+
+    this.head.style.left = value + "px";
   }
 
-  set Y(value:number){
-    this.head.style.top = value+"px"
+  set Y(value: number) {
+    if (this.Y === value) {
+      return;
+    }
+    if (value < 0 || value > 290) {
+      throw new Error("蛇撞牆了");
+    }
+    this.head.style.top = value + "px";
   }
 
-
-  addBody(){
-    this.element.insertAdjacentHTML("beforeend","<div></div>")
+  addBody() {
+    this.element.insertAdjacentHTML("beforeend", "<div></div>");
   }
 }
 
